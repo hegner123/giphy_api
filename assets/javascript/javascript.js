c70f2888308f3b5f746ddc2b3af2d9f57c7029bd
Michael Hegner <hegner123@gmail.com>
2019-09-11 12:55:07 -0400

    completed user input functionality. Completed user input clear after submit. completed new button. completed new button functionality.

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,53 +1,44 @@
 // api key uAqq2e9vPXAPuNoT7BAQMESLv2UA1RYd
-
-
-
-
 $(document).ready(function (){
-
-$('.gif-button').on('click',function (){
-  var queryURL = 'https://api.giphy.com/v1/gifs/search?q=pandas&api_key=uAqq2e9vPXAPuNoT7BAQMESLv2UA1RYd';
-  $.ajax ({
-    url:queryURL,
-    method: 'GET',
-  })
-    .then(function(response){
-      var results = response.data;
-      for (i=0;i<20;i++){
-        var imageDiv = $('<div class="ml-3">')
-        var imageImg = $('<img src="">')
-        imageImg.attr("src", results[i].images.fixed_height.url)
-        imageDiv.appendTo('.display');
-        imageImg.appendTo(imageDiv);
-
-      }
-    })
-})
-
-// $("#search-btn").on("click", function (){
   
-//   var searchTerm = $("#search-term").val();
-//   var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTerm + "&api-key=AFlvZG0WQAGK0M8a8zXNmxv8ZkCfp7fq";
-//   $.ajax({
-//     url: queryURL,
-//     method: "GET",
-//   })
-//       .then(function(response) {
-//         var results = response.response.docs;
-        
-        
-//         for(i= 0 ;i<results.length;i++){
-//           var br = $("<br>");
-//           var article = $("<div>").addClass("article" + [i])
-//           article.text(results[i].snippet);
-//           article.prependTo(".articles");
-//           br.prependTo(".articles");
+clickStuff();
 
-//           searchTerm= "";
-//         }
-      
-//       })
-// })
+  $('#add-button').on('click', function (){
+    var userInput = $("#user-input").val()
+    console.log(userInput);
+    var newButton = $('<button class="btn gif-button ml-2" data-topic="">')
+    newButton.attr("data-topic", userInput );
+    newButton.text(userInput);
+    newButton.appendTo(".button-div");
+    clickStuff();
+    $(".user-input-text").val("");
+  });
+
+  
+
+  function clickStuff(){
+    $('.gif-button').click(function (){
+      console.log("this");
+      var searchTerm = $(this).attr("data-topic");
+      var queryURL = 'https://api.giphy.com/v1/gifs/search?q=' + searchTerm + '&api_key=uAqq2e9vPXAPuNoT7BAQMESLv2UA1RYd';
+      $.ajax ({
+        url:queryURL,
+        method: 'GET',
+      })
+        .then(function(response){
+          var results = response.data;
+          $('.display').empty();
+          for (i=0;i<20;i++){
+            var imageDiv = $('<div class="ml-3">')
+            var imageImg = $('<img src="">')
+            imageImg.attr("src", results[i].images.fixed_height.url)
+            imageDiv.appendTo('.display');
+            imageImg.appendTo(imageDiv);
+    
+          }
+        })
+    });
+  };
 
 
 
